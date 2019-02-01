0b9e538fe9c730a4c33b5bb341a38defe95c3b32
Ram Iyer <riyer@teladoc.com>
2019-02-01 12:28:45 -0500

    starting graphql server with new ApolloServer syntax

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
 const expressJwt = require('express-jwt');
+const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const db = require('./db');
 
@@ -9,20 +9,56 @@ const port = 9000;
 const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
 
 const app = express();
-app.use(cors(), bodyParser.json(), expressJwt({
-  secret: jwtSecret,
-  credentialsRequired: false
-}));
+const typeDefs = fs.readFileSync('./schema.graphql', {encoding: 'utf-8'});
+const resolvers = require('./resolvers');
 
-app.post('/login', (req, res) => {
-  const {email, password} = req.body;
-  const user = db.users.list().find((user) => user.email === email);
-  if (!(user && user.password === password)) {
-    res.sendStatus(401);
-    return;
-  }
-  const token = jwt.sign({sub: user.id}, jwtSecret);
-  res.send({token});
+const server = new ApolloServer({
+  typeDefs, resolvers
 });
+server.applyMiddleware({ app });
 
-app.listen(port, () => console.info(`Server started on port ${port}`));
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
+)
+
+
+//=====
+
+// const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+// const { makeExecutableSchema } = require('graphql-tools');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const express = require('express');
+// const expressJwt = require('express-jwt');
+// const fs = require('fs');
+// const jwt = require('jsonwebtoken');
+// const db = require('./db');
+
+// const port = 9000;
+// const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
+
+// const typeDefs = fs.readFileSync('./schema.graphql', {encoding: 'utf-8'});
+// const resolvers = require('./resolvers');
+// const schema = makeExecutableSchema({typeDefs, resolvers});
+
+// const app = express();
+// app.use(cors(), bodyParser.json(), expressJwt({
+//   secret: jwtSecret,
+//   credentialsRequired: false
+// }));
+
+// app.use('/graphql', graphqlExpress({schema}));
+// app.use('/graphiql', graphiqlExpress({endpointURL: '/graphql'}));
+
+// app.post('/login', (req, res) => {
+//   const {email, password} = req.body;
+//   const user = db.users.list().find((user) => user.email === email);
+//   if (!(user && user.password === password)) {
+//     res.sendStatus(401);
+//     return;
+//   }
+//   const token = jwt.sign({sub: user.id}, jwtSecret);
+//   res.send({token});
+// });
+
+// app.listen(port, () => console.info(`Server started on port ${port}`));
